e2de94bd4186179bcc2ac2e07f901c615c172363
Jon <58070934+Jon811@users.noreply.github.com>
2025-12-07 16:26:34 +0000

    4

--- torrserver/run.sh
+++ torrserver/run.sh
@@ -1,28 +1,10 @@
 #!/usr/bin/env bashio
-# torrserver/run.sh
 
-bashio::log.info "Starting TorrServer add-on..."
-
-# Ждем немного для инициализации
-sleep 2
-
-# Получаем параметры из конфигурации
-PORT=$(bashio::config 'port' '8090')
-STREAM_PORT=$(bashio::config 'stream_port' '8091')
-TORRENT_PORT=$(bashio::config 'torrent_port' '49165')
-CACHE_SIZE=$(bashio::config 'cache_size' '512')
-LOG_LEVEL=$(bashio::config 'log_level' 'info')
-
-# Формируем команду запуска
-CMD="/app/TorrServer \
-    -p ${PORT} \
-    -s ${STREAM_PORT} \
-    -t ${TORRENT_PORT} \
-    -ct ${CACHE_SIZE} \
-    -ll ${LOG_LEVEL} \
-    -d /data/torrserver"
-
-bashio::log.info "Command: ${CMD}"
-
-# Запускаем TorrServer
-exec ${CMD}
+# Просто запускаем с дефолтными параметрами если проблемы
+/app/TorrServer \
+    -p 8090 \
+    -s 8091 \
+    -t 49165 \
+    -ct 512 \
+    -ll info \
+    -d /data/torrserver
