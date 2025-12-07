f746171762f08c63838d3588f54c0b847269dc43
Jon <58070934+Jon811@users.noreply.github.com>
2025-12-07 16:05:54 +0000

    1

--- torrserver/run.sh
+++ torrserver/run.sh
@@ -1,17 +1,18 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
 
-bashio::log.info "Starting TorrServer..."
+# Ждем готовности системы
+sleep 3
 
-# Базовые параметры
-PORT=$(bashio::config 'port' 8090)
-STREAM_PORT=$(bashio::config 'stream_port' 8091)
-TORRENT_PORT=$(bashio::config 'torrent_port' 49165)
-CACHE_SIZE=$(bashio::config 'cache_size' 512)
+# Базовые параметры (всегда требуются)
+PORT=$(bashio::config 'port' "8090")
+STREAM_PORT=$(bashio::config 'stream_port' "8091")
+TORRENT_PORT=$(bashio::config 'torrent_port' "49165")
+CACHE_SIZE=$(bashio::config 'cache_size' "512")
 
-# Запуск
+# Запускаем
 exec /app/TorrServer \
-  -p "${PORT}" \
-  -s "${STREAM_PORT}" \
-  -t "${TORRENT_PORT}" \
-  -ct "${CACHE_SIZE}" \
-  -d /data/torrserver
+    -p "${PORT}" \
+    -s "${STREAM_PORT}" \
+    -t "${TORRENT_PORT}" \
+    -ct "${CACHE_SIZE}" \
+    -d /data/torrserver
