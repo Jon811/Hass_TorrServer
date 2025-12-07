c233efa0f4d653508aabd7f59f43bb6bff1f3adf
Jon <58070934+Jon811@users.noreply.github.com>
2025-12-07 16:16:41 +0000

    1

--- torrserver/run.sh
+++ torrserver/run.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/with-contenv bashio
 
-# Просто запускаем с параметрами из HA
-/app/TorrServer \
-    -p "$(bashio::config 'port' '8090')" \
-    -s "$(bashio::config 'stream_port' '8091')" \
+# Получаем параметры из конфигурации Home Assistant
+PORT=$(bashio::config 'port')
+STREAM_PORT=$(bashio::config 'stream_port')
+TORRENT_PORT=$(bashio::config 'torrent_port')
+CACHE_SIZE=$(bashio::config 'cache_size')
+
+# Устанавливаем значения по умолчанию
+: "${PORT:=8090}"
+: "${STREAM_PORT:=8091}"
+: "${TORRENT_PORT:=49165}"
+: "${CACHE_SIZE:=512}"
+
+# Запускаем TorrServer
+exec /app/TorrServer \
+    -p "${PORT}" \
+    -s "${STREAM_PORT}" \
+    -t "${TORRENT_PORT}" \
+    -ct "${CACHE_SIZE}" \
     -d /data/torrserver
